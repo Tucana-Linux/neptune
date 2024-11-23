90c91d3153502e30134db4cccb5ab07d3911ea89
Rahul Chandra <rahul@projecteclipse.org>
2024-11-23 17:42:36 -0500

    Implement native mercury sync Fixes #5

--- src/neptune/sync.py
+++ src/neptune/sync.py
@@ -1,3 +1,21 @@
 import subprocess
+import os
+import requests
+from neptune import functions
+# initally refactored from mercury-sync
 def sync():
-    subprocess.run("mercury-sync", shell=True)
+    os.chdir(f'{functions.cache_dir}/depend')
+    print("Getting Available Packages")
+    available_packages=requests.get(f"{functions.repo}/available-packages/packages")
+    open(f'{functions.cache_dir}/available-packages', 'wb').write(available_packages.content)
+
+    print("Getting dependency files")
+    depends=requests.get(f"{functions.repo}/available-packages/packages")
+    open(f'{functions.cache_dir}/depend/depends.tar.xz', 'wb').write(depends.content)
+    subprocess.run('tar -xpf depends.tar.xz', shell=True)
+
+    print("Getting meta info")
+    sha256=requests.get(f"{functions.repo}/available-packages/packages")
+    open(f'{functions.cache_dir}/sha256', 'wb').write(sha256.content)
+
+
