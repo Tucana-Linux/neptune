9474788b076cfb143fe315c697d6a3463dc90396
Rahul Chandra <rahul@projecteclipse.org>
2024-12-22 18:50:34 -0500

    Forgot --y & wrong file location

--- tests/test.sh
+++ tests/test.sh
@@ -361,7 +361,7 @@ function reinstall_test() {
   # If this test passes than we know that file io while the file is open is already working
   make_mock_package "reinstall-test" "" "" ""
      # Create a script to keep the file open
-    cat > $TEMP_DIR/keep_file_open.py << EOF
+    cat > $CHROOT/keep_file_open.py << EOF
 import time
 import sys
 
@@ -375,14 +375,14 @@ with open('/tests/reinstall-test/reinstall-test', 'r') as f:
 EOF
 
     # Run file-keeping script in background
-    chroot $CHROOT /bin/bash -c "python3 /tmp/keep_file_open.py" &
+    chroot $CHROOT /bin/bash -c "python3 /keep_file_open.py" &
     KEEP_FILE_PID=$!
 
     # Wait a moment to ensure file is opened
     sleep 2
 
     # Attempt to reinstall while file is open
-    chroot $CHROOT /bin/bash -c "neptune reinstall open-file-test"
+    chroot $CHROOT /bin/bash -c "neptune reinstall --y open-file-test"
 
     # Check if reinstall succeeded
     if [[ $? == 0 ]]; then
