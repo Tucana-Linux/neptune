9863bde563a54fbfed07f19b69762bc58f1b0a70
Rahul Chandra <rahul@projecteclipse.org>
2024-12-22 20:00:52 -0500

    switch logic

--- tests/test.sh
+++ tests/test.sh
@@ -61,13 +61,8 @@ function chroot_setup() {
   # TODO Change to /var/lib/neptune once neptune is finalized Rahul Chandra <rahul@tucanalinux.org>
   mkdir -p $CHROOT/var/cache/mercury/file-lists
   chroot $CHROOT /bin/bash -c "mercury-install --y python-urllib3 python-requests pyyaml"
-  # needed until we completely switch to neptune
-  cat > $CHROOT/etc/wanted_packages << EOF
-base-update
-python-urllib3
-python-requests
-pyyaml
-EOF
+  # remove the installed_package so it can be fresh neptune
+  echo "" > $CHROOT/etc/installed_package
 }
 
 function setup() {
