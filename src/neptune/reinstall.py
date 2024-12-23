313364980946852f51d1fdd2a7656b187d33345f
Rahul Chandra <rahul@projecteclipse.org>
2024-12-22 19:35:29 -0500

    Exit if package is not installed

--- src/neptune/reinstall.py
+++ src/neptune/reinstall.py
@@ -20,4 +20,8 @@ def reinstall():
       if not (confirmation=="y" or confirmation=="" or confirmation == "Y"):
          print("Aborting")
          sys.exit(0)
+   for package in functions.arguments:
+      if package not in functions.installed_packages:
+         print(f'{package} is not installed!')
+         sys.exit(1)
    functions.install_packages(functions.arguments, "other", reinstalling=True)
