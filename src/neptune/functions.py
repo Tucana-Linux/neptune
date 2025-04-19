006cc55dbe88d8ca803c8e9338cedac5e9c5c2fc
Rahul Chandra <rahul@projecteclipse.org>
2025-04-19 01:27:20 -0400

    fix ospath join

--- src/neptune/functions.py
+++ src/neptune/functions.py
@@ -60,7 +60,7 @@ def generate_file_list(package):
     os.chdir(f'{settings.cache_dir}/{package}')
     # This is a bash oneliner, I know it isn't ideal but it's easier to read than the python alternative
     subprocess.run(f'find * -type f | sed \'s|^|/|g\' > /tmp/{package}.list', shell=True)
-    if os.path.isfile(os.path(f"{settings.lib_dir}/file-lists/{package}.list")):
+    if os.path.isfile(os.path.join(f"{settings.lib_dir}/file-lists/{package}.list")):
       remove_old_files(package, f"/tmp/{package}.list")
     subprocess.run(f'cp -rp /tmp/{package}.list {settings.lib_dir}/file-lists/{package}.list')
     # Remove the files that need to be backed up from the file-list so that they aren't removed
